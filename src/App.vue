14c8e1816151cf34b851178190dc120a2abae599
David Dume <dumedavid11@gmail.com>
2023-03-24 15:29:55 +0100

    aggiunta jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
-  import AppTitle from './components/AppTitle.vue'
+  import AppHero from './components/AppHero.vue'
+  import AppTitle from './components/AppTitle.vue';
 
   export default {
     components: {
+      AppHero,
       AppTitle
     }
   }
 </script>
 
 <template>
-  <AppTitle></AppTitle>
+  <AppTitle/>
+  <AppHero />
 </template>
 
 <style scoped>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,18 @@
+
+
+<template>
+    <div class="jumbotron">
+        <h1>
+            Jumbotron
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+    .jumbotron {
+        width: 600px;
+        height: 600px;
+        background-color: white;
+        color: black;
+    }
+</style>
